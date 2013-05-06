8e77ae38f2ebf1bca9e543617f4aba3a09223afd
Andreas Kloeckner <inform@tiker.net>
2013-05-06 11:02:07 -0400

    Add Jan to copyright. (Duh!)

--- pydgeon/local.py
+++ pydgeon/local.py
@@ -1,5 +1,5 @@
 # Pydgeon - the Python DG Environment
-# (C) 2009, 2010 Tim Warburton, Xueyu Zhu, Andreas Kloeckner
+# (C) 2009, 2010 Tim Warburton, Jan Hesthaven, Xueyu Zhu, Andreas Kloeckner
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
